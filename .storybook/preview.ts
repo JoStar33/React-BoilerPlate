93ea7da7ca537e5fec61069d5fc7232edc76d7ca
HoCG <rhwkd5012@naver.com>
2023-04-29 16:59:41 +0900

    feat: 기존 아토믹 버튼 리액트에 마이그레이션

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,16 @@
 import type { Preview } from "@storybook/react";
+import { ThemeProvider } from "styled-components";
+import { setupWorker } from "msw";
+import { handlers } from "../src/mocks/handlers";
+
+// if (typeof global.process === "undefined") {
+//   //checks to make sure that this is not a node process
+//   const worker = setupWorker(
+//     //create service worker
+//     ...handlers
+//   );
+//   worker.start(); // worker starts!
+// }
 
 const preview: Preview = {
   parameters: {
